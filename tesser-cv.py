4514b2621d8f88a4182484e4c4a8da920fe0adf9
zixsorus <touy_elec@hotmail.com>
2020-08-12 10:14:52 +0700

    rename

--- tesser-cv.py
+++ tesser-cv.py
@@ -2,7 +2,7 @@ import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-img = cv2.imread('py4.png')
+img = cv2.imread('image\\py1.png')
 
 text = pytesseract.image_to_string(img)
 print(text)
--- tesser.py
+++ tesser.py
@@ -1,4 +1,4 @@
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract'
 
-print(pytesseract.image_to_string(r'D:\\Git\\py-text\\py3.png'))
+print(pytesseract.image_to_string(r'D:\\Git\\py-text\\image\\py1.png'))
